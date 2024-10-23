b4ab1f4a3112aeba19c08f55eda066f24cf1118f
LightQWE <rpappra@gmail.com>
2024-10-23 23:04:17 +0500

    домашкаа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,61 @@
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
+        for(int i = 0; i <3;i++){
+            Car car = new Car();
+            boolean True = true;
+            while(True) {
+                System.out.println("Введите скорость " + (i + 1) + " машины: ");
+                int speedCar = scanner.nextInt();
+                if (speedCar <= 250 && speedCar > 0) {
+                    car.speed = speedCar;
+                    System.out.println("Введите имя " + (i + 1) + " машины: ");
+                    car.name = scanner.next();
+                    cars.add(car);
+                    True = false;
+                }
+                else{
+                    System.out.println("Скорость введена не верно");
+                }
+            }
+
+        }
+        Race race = new Race();
+        race.Winner(cars);
+
+        System.out.println("Самая быстрая машина: " + race.nameCar);
+
+    }
+
+}
+class Car {
+    Scanner scanner = new Scanner(System.in);
+    String name;
+    int speed;
+    public void Car(String name,int speed){
+        this.name = name;
+        this.speed = speed;
+    }
+}
+class Race {
+    String nameCar ="";
+    int dist = 0;
+    public void Winner(ArrayList<Car> cars){
+        for(Car car : cars) {
+            int dist1 = 24 * car.speed;
+            if (dist < dist1) {
+                dist = dist1;
+                nameCar = car.name;
+            }
+        }
+
+    }
+    public String name(){
+
+        return nameCar;
     }
 }
